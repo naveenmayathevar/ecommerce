3c71e72a125d4b8c6c717949a53cc7ae56f3a930
Naveen Mayathevar <naveenmayathevar@gmail.com>
2026-06-07 23:42:02 -0700

    fix: set no-unused-vars to warn

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js'
+﻿import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -23,8 +23,9 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
 ])
+
 
